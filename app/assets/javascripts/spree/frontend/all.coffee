89b16f30ac4080ac3c6d56cdcb1502be48915667
Caroline Russell <carolinerussell2017@gmail.com>
2018-06-12 13:57:51 -0400

    First upload

---
+++ app/assets/javascripts/spree/frontend/all.coffee
@@ -0,0 +1,8 @@
+#= require jquery
+#= require jquery_ujs
+#= require bootstrap
+#= require spree
+#= require spree/frontend/checkout
+#= require spree/frontend/product
+#= require spree/frontend/cart
+#= require spree/frontend/locale_selector
---
+++ app/helpers/spree/base_helper_decorator.rb
@@ -0,0 +1,27 @@
+module Spree::BaseHelper
+  def layout_partial
+    if devise_controller?
+      'spree/base/devise'
+    else
+      'spree/base/application'
+    end
+  end
+
+  def logo(image_path = Spree::Config[:logo], img_options: {})
+    link_to image_tag(image_path, img_options), spree.root_path
+  end
+end
+
+def nav_tree(root_taxon, current_taxon, max_level = 1)
+  return '' if max_level < 1 || root_taxon.children.empty?
+  content_tag :ul, class: 'dropdown-menu' do
+    taxons = root_taxon.children.map do |taxon|
+      css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'active' : nil
+      content_tag :li, class: css_class do
+       link_to(taxon.name, seo_url(taxon)) +
+         taxons_tree(taxon, current_taxon, max_level - 1)
+      end
+    end
+    safe_join(taxons, "\n")
+  end
+end
